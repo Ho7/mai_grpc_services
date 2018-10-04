76ad1167a8f10f96ebf7a074764c3fe06ed75253
ho7 <okunev.mtu@mail.ru>
2018-10-04 09:58:01 +0300

    допили рест апи на ноде

--- sale_client.js
+++ sale_client.js
@@ -1,9 +1,14 @@
 var grpc = require('grpc');
-
+var express = require('express');
+var app = express();
 var saleProto = grpc.load('sales.proto');
 
 var sale_client = new saleProto.CalculatorSale('127.0.0.1:8001', grpc.credentials.createInsecure());
 
+app.get('/discounts', function (req, res) {
+        var cost = getSale(12);
+       res.end(JSON.stringify(cost));
+});
 
 function printResponse(error, response) {
     if (error)
@@ -16,7 +21,10 @@ function getSale(cost) {
     sale_client.getSale(cost, function(error, cost) {
         printResponse(error, cost);
     });
+    return {'cost': cost}
 }
 
-console.log(getSale(12));
+var server = app.listen(8081, function () {
 
+
+});
